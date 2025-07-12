9e92541c45f3b15ed022ef7281e01e911918efc6
Marian Negru <marian.negru@gmail.com>
2025-07-12 06:21:28 +0000

    new journey content has a blank space on top can you check it and remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
 
   return (
       <AppLayout activeView={activeView} setActiveView={setActiveView}>
-        <div className="flex flex-col flex-1 p-4 sm:p-8 pt-6">
+        <div className="flex flex-col flex-1 p-4 sm:p-8">
           {renderContent()}
         </div>
       </AppLayout>
