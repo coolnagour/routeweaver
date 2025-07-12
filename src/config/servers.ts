58d9ab455684ba7911022417a52676c5a11a600e
Marian Negru <marian.negru@gmail.com>
2025-07-12 20:05:59 +0000

    2025-07-12T20:03:59Z [web] [iCabbi API Request] ---> GET https://8stagin

--- src/config/servers.ts
+++ src/config/servers.ts
@@ -13,7 +13,7 @@ export const servers: ServerConfig[] = [
         name: '8 Staging - Mango Cabs S3 - Gerry',
         host: '8stagingapi.icabbi.com',
         apiPath: '8staging',
-        apiKey: '3b79a230ee7532cbd250704e707f3021d7d271e3',
+        appKey: '3b79a230ee7532cbd250704e707f3021d7d271e3',
         secretKey: '15fc2161e049a49044ccfb3fd048359bf0cba68d',
         companyId: '1100'
     },
