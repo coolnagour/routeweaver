4acbca93c236719467791b87652d959fa5a27647
Marian Negru <marian.negru@gmail.com>
2025-07-12 06:26:26 +0000

    is something wrong with sidebar content is rendered on bottom of the pag

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -14,7 +14,7 @@ interface AppLayoutProps {
 export default function AppLayout({ children, activeView, setActiveView }: AppLayoutProps) {
   return (
     <SidebarProvider>
-      <div className="relative min-h-screen">
+      <div className="relative flex min-h-screen">
         <Sidebar collapsible="icon">
           <MainSidebar activeView={activeView} setActiveView={setActiveView} />
         </Sidebar>
